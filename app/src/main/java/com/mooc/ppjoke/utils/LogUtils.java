37886e6108c3a7f23ca6e209702606b7bf4aaa27
liaolexian <liaolexian@vipc.cn>
2023-04-18 10:39:42 +0800

    Initial commit

---
+++ app/src/main/java/com/mooc/ppjoke/utils/LogUtils.java
@@ -0,0 +1,12 @@
+package com.mooc.ppjoke.utils;
+
+import android.util.Log;
+
+public class LogUtils {
+
+    private static final String TAG = "LOG";
+
+    public static void e(String s) {
+        Log.e(TAG,s);
+    }
+}
